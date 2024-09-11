39abb0a51c398f1cdf20b5c73e25e535688cec69
martinbetancur <martinjavieli@gmail.com>
2024-09-11 08:07:15 -0500

    Listo

--- vuelos/urls.py
+++ vuelos/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'vuelos'
+
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('register/', views.register_flight, name='register_flight'),
+    path('', views.register_flight, name='register_flight'),
     path('list/', views.list_flights, name='list_flights'),
     path('stats/', views.flight_stats, name='flight_stats'),
 ]
